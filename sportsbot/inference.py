bf193aa0c762a936d5fe9b77ceded6632406e80d
Charysse <charysse.redwood@gmail.com>
2020-09-26 13:23:51 -0700

    change for testing

--- sportsbot/inference.py
+++ sportsbot/inference.py
@@ -49,6 +49,7 @@ def few_shot_train(data,
         top_softmax = _top_softmax(predicted_prob,tokenizer)
         confidence[f"{i+1}_test"] = top_softmax
         _add_stats(conversations[i],top_softmax,jsonlines_file)
+    print(len(model_answers))
     accuracy = _calculate_accuracy(labels, model_answers)
     statistics = {
                     "accuracy": accuracy,
