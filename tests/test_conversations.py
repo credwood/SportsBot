1775c2de31630244ee116cb5eac5894c8bcf24de
Charysse <charysse.redwood@gmail.com>
2020-09-29 13:49:48 -0700

    deleted test case

--- tests/test_conversations.py
+++ tests/test_conversations.py
@@ -6,11 +6,6 @@ class TestTweepyStream(unittest.TestCase):
     """
     these functions will cerate a .jsonl output file
     """
-    def test_get_convs(self):
-        filter_out= ['"bad words', "bad", "words"]
-        search = '"grazing goats"'
-        get_conversations(search, filter_out)
-    
     def test_likely_long_convs(self):
         filter_out = []
         search = "I"
