7fddb651b62bcc9ce6fcd90937526327cc905a00
Charysse <charysse.redwood@gmail.com>
2020-09-30 13:18:11 -0700

    added template 'test'

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,6 +1,7 @@
 import unittest
 from sportsbot.datasets import Tweet, ConversationPrompt
-from sportsbot.datasets import _save_data, _read_data
+from sportsbot.datasets import _save_data, _read_data, _prepare_testing_set
+from sportsbot.conversations import get_conversations
 
 class TestJsonlinesFunctions(unittest.TestCase):
     def test_encode_decode_data(self):
@@ -9,7 +10,8 @@ class TestJsonlinesFunctions(unittest.TestCase):
                                 [Tweet(12338473830332,
                                         "dog",
                                         "dooog",
-                                        "@TheHoopGenius I heard someone say AD better than Giannis 😂😂😂",
+                                        "@TheHoopGenius I heard someone say AD"\
+                                             "better than Giannis 😂😂😂",
                                         "en",
                                         "Tue Mar 29 08:11:25 +0000 2011",
                                         122,
@@ -19,3 +21,12 @@ class TestJsonlinesFunctions(unittest.TestCase):
         _save_data([test_conv_obj], "read_write_test.jsonl")
 
         self.assertEqual(test_conv_obj, _read_data("read_write_test.jsonl")[0])
+
+class TestTemplateFunctions(unittest.TestCase):
+    conversations = get_conversations('"lakers suck"', ["china", "racist"])
+    training_convs = conversations[:3]
+    training_labels = ["Yes"]*3
+    testing_convs = conversations[3:]
+    templates = _prepare_testing_set(training_convs, testing_convs, "lakers", training_labels)
+    for conv in templates:
+        print(conv)
