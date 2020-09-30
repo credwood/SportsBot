25e44282f964f1e9fb687ce34526207ac3f5ba5a
Charysse <charysse.redwood@gmail.com>
2020-09-30 15:43:17 -0700

    updated template printing test case

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -27,6 +27,6 @@ class TestTemplateFunctions(unittest.TestCase):
     training_convs = conversations[:3]
     training_labels = ["Yes"]*3
     testing_convs = conversations[3:]
-    _,templates = _prepare_testing_set(training_convs, testing_convs, "lakers", training_labels)
-    for conv in templates:
+    full_templates,_ = _prepare_testing_set(training_convs, testing_convs, "lakers", training_labels)
+    for conv in full_templates:
         print(conv)
