1096e165cc215ab96be80ec3c90a8695055fca62
Charysse <charysse.redwood@gmail.com>
2020-12-09 13:14:17 -0800

    fixed stagnant training

--- sportsbot/finetune.py
+++ sportsbot/finetune.py
@@ -2,7 +2,7 @@ import random
 import os
 import torch
 
-from transformers import GPT2Tokenizer, GPT2LMHeadModel, AdamW, get_linear_schedule_with_warmup
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, AdamW, Adafactor, get_linear_schedule_with_warmup
 from tqdm import tqdm
 from torch.nn.utils.rnn import pad_sequence
 #import torch.nn.functional as F
@@ -12,6 +12,7 @@ def train(
     dataset,
     validation_set=None,
     validation_labels=None,
+    labels_dict=None,
     model=GPT2LMHeadModel,
     tokenizer=GPT2Tokenizer,
     batch_size=1,
@@ -29,7 +30,7 @@ def train(
 ):
 
     # We can add these special tokens to the vocabulary and the embeddings of the model:
-    tokenizer = tokenizer.from_pretrained(gpt2_type,pad_token="<pad>")
+    tokenizer = tokenizer.from_pretrained(gpt2_type,pad_token="<pad>", padding_side="left")
     model = model.from_pretrained(gpt2_type,pad_token_id=tokenizer.pad_token_id)
     #acc_steps = 100
 
@@ -37,6 +38,7 @@ def train(
     model.train()
 
     optimizer = AdamW(model.parameters(), lr=lr,weight_decay=0.0)
+    #optimizer = Adafactor(model.parameters())
     scheduler = get_linear_schedule_with_warmup(
         optimizer, num_warmup_steps=warmup_steps, num_training_steps=-1
     )
@@ -46,33 +48,37 @@ def train(
 
     for epoch in range(epochs):
 
+        model.train()
         random.shuffle(dataset)
 
         print(f"Training epoch {epoch}")
-        batched_dataset = create_batches(dataset,batch_size,max_seq_len)
+        #batched_dataset = create_batches(dataset,batch_size,max_seq_len)
+        batched_dataset = [conv for conv in dataset if len(conv.template) < max_seq_len]
         for index, batch in tqdm(enumerate(batched_dataset),ascii=True, desc="current batch"):
             """
             stuffing tensors:
                 (input_tensor, carry_on, remainder) = pack_tensor(entry, input_tensor, 1024)
-
                 if carry_on and idx != len(train_dataloader) - 1:
                     continue
             """
             #input_tensor = input_tensor.to(device)
             batched_tensors, labels, mask = tokenize_data(batch, tokenizer, device)
-            outputs = model(batched_tensors, attention_mask=mask, labels=labels)
+            outputs = model(batched_tensors, labels=labels) #
             loss = outputs[0]
+            if (index+1)%99==0 or index == len(dataset)-1:
+                print(f"loss on the ({index}+1)%99 iteration: {loss}")
             loss.backward()
 
-            if (index+1)%16 or index == len(dataset)-1:
+            if (index+1)%batch_size==0 or index == len(dataset)-1:
                 optimizer.step()
                 scheduler.step()
                 optimizer.zero_grad()
                 model.zero_grad()
+              
 
         if eval_between_epochs:
-            predict(validation_set,tokenizer, model,json_file_out=f"validation_{epoch}.json",labels=validation_labels)
-            #accumulating_batch_count += 1
+            model.eval()
+            predict(validation_set,tokenizer, model,json_file_out=f"validation_{epoch}.json",labels=validation_labels, labels_dict=labels_dict)
         if save_model_on_epoch:
             torch.save(
                 model.state_dict(),
@@ -81,30 +87,36 @@ def train(
     return model
 
 def create_batches(data, batch_size,max_seq_len):
-    data = sorted(data, key=lambda x: len(x.template))
-    data = [record for record in data if len(record.template) <= max_seq_len]
-    data = [data[index:index+batch_size] for index in range(0,len(data), batch_size)]
-    random.shuffle(data)
-    return data
+    data_ = sorted(data, key=lambda x: len(x.template))
+    data_ = [record for record in data_ if len(record.template) <= max_seq_len]
+    data_ = [data_[index:index+batch_size] for index in range(0,len(data_), batch_size)]
+    random.shuffle(data_)
+    return data_
 
-def tokenize_data(conversations, tokenizer, device):
+def tokenize_data(conversation, tokenizer, device):
     """returns a list of tokenized torch tensors"""
     # padding_value can be whatever...
-    tokens = [[tokenizer.encode(conv.template) for conv in conversations] ]
-    inputs = pad_sequence([torch.LongTensor(template) for template in tokens], padding_value=tokenizer.pad_token_id).to(device)
+    tokens = tokenizer.encode(conversation.template)
+    label_check = " " + conversation.template.split(" ")[-1]
+    assert len(tokenizer.encode(label_check)) == 1
+    assert label_check == tokenizer.decode(tokens[-1])
+    inputs = torch.LongTensor(tokens).to(device)
+    #inputs = pad_sequence([torch.LongTensor(template) for template in tokens], padding_value=tokenizer.pad_token_id).to(device)
     # 1 for real tokens and 0 for padded tokens
     mask = (inputs != tokenizer.pad_token_id).float()
     #for the labels, pad conversation part of template with -100
-    label_mask = []
-    for conversation in conversations:
-        conv_thread = ""
-        for tweet in conversation.thread:
-            conv_tread+= f"{tweet.user_handle}: {tweet.content}\n"
-        label_mask.append(len(tokenizer.encode(conv_thread)))
-    token_labels = [[-100 if index < label_mask[i] else tokens[i][index] for index in range(len(tokens[i]))] for i in range(len(tokens))]
-    labels = pad_sequence([torch.LongTensor(template) for template in token_labels], padding_value=tokenizer.pad_token_id).to(device)
+    #label_mask = []
+    #for conversation in conversations:
+        #conv_thread = ""
+        #for tweet in conversation.thread:
+            #conv_thread+= f"{tweet.user_handle}: {tweet.content}\n"
+        #label_mask.append(len(tokenizer.encode(conv_thread)))
+    #token_labels = [[-100. if index < label_mask[i] else tokens[i][index] for index in range(len(tokens[i]))] for i in range(len(tokens))]
+    #labels = pad_sequence([torch.LongTensor(template) for template in token_labels], padding_value=tokenizer.pad_token_id).to(device)
+    labels = inputs[:]
+    # replace the ids of the padded tokens (where token_id==padded_id) with `-1`
+    #labels = inputs.masked_fill(inputs == tokenizer.pad_token_id, -1)
     return inputs, labels, mask
-
 
 def pack_tensor(new_tensor, packed_tensor, max_seq_len):
     if packed_tensor is None:
--- sportsbot/inference.py
+++ sportsbot/inference.py
@@ -3,6 +3,7 @@ Functions for testing Hugginface's GPT-2 trained model
 """
 import json
 from collections import defaultdict
+from numpy import exp, log
 import torch
 import torch.nn.functional as F
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
@@ -70,7 +71,7 @@ def few_shot_test(test_data,
     _save_data(templated_conversations,jsonlines_file_out)
 
     if test_labels:
-        accuracy = _calculate_accuracy(test_labels, model_answers)
+        accuracy = _calculate_accuracy(test_labels, model_answers, labels_dict)
         statistics = {
                         "accuracy": accuracy,
                         "confidence_dict": confidence,
@@ -87,13 +88,14 @@ def predict(test_convs,
             device="cuda",
             num_top_softmax=20,
             json_file_out='add_stats_output.jsonl',
-            labels=None
+            labels=None,
+            labels_dict=None
         ):
     """
     Function for finetuned model. saves and returns `Conversation` objects
     with model statistics (top SoftMax values for each conversation).
     """
-    model.eval()
+    assert  sum([len(tokenizer.encode(label)) == 1 for label in labels_dict.values()]) == len(list(labels_dict.values()))
     model.to(device)
     conversations = defaultdict()
     answers = []
@@ -108,36 +110,40 @@ def predict(test_convs,
         top_softmax = _top_softmax(predicted_prob, tokenizer, num_top_softmax)
         answers.append(top_softmax[0][0])
         if labels:
-            softmax_tokens = _label_softmax(predicted_prob, tokenizer, [' No',' N/A',' Unlikely',' Maybe',' Probably',' Yes'])
-            conversations[i] = [test_conv, softmax_tokens, labels[i], top_softmax]
+            softmax_tokens = _label_softmax(predicted_prob, tokenizer, labels_dict)
+            conversations[str(i)] = [test_conv, softmax_tokens, labels[i], top_softmax]
         else:
-            softmax_tokens = _label_softmax(predicted_prob, tokenizer, [' No',' N/A',' Unlikely',' Maybe',' Probably',' Yes'])
-            conversations[i] = [test_conv, softmax_tokens, top_softmax]
+            softmax_tokens = _label_softmax(predicted_prob, tokenizer, labels_dict)
+            conversations[str(i)] = [test_conv, softmax_tokens, top_softmax]
     if labels:
-        conversations["accuracy"] = str(_calculate_accuracy(labels, answers))
+        conversations["accuracy"] = str(_calculate_accuracy(labels, answers, labels_dict))
+        print(f"accuracy: {conversations['accuracy']}")
     with open(json_file_out, "w") as dump:
         json.dump(conversations, dump, indent=4)
     return conversations
 
-def _calculate_accuracy(labels, model_answers):
+def _calculate_accuracy(labels, model_answers, labels_dict):
     correct = 0.
     for i, answer in enumerate(labels):
-        if  model_answers[i] == answer:
+        if  labels_dict[answer] == model_answers[i]:
             correct += 1
     return correct/len(labels)
 
 def _top_softmax(prob_dict, tokenizer, num_tokens):
     num_tokens = min(len(prob_dict), num_tokens)
     _, sorted_indices = torch.sort(prob_dict[:], descending=True)
-    sorted_indices = list(sorted_indices.detach().numpy())
+    sorted_indices = list(sorted_indices.cpu().numpy())
     return [(tokenizer.decode([index]), str(prob_dict[index])) for index in sorted_indices[:num_tokens]]
 
-def _label_softmax(prob_dict, tokenizer, labels_lst):
-    prob_dict = list(prob_dict.detach().numpy())
-    return [{label: str(prob_dict[tokenizer.encode(label)[0]])} for label in labels_lst]
+def _label_softmax(prob_dict, tokenizer, labels_dict):
+    prob_dict = list(prob_dict.detach().cpu().numpy())
+    return [{label: str(prob_dict[tokenizer.encode(label)[0]])} for label in list(labels_dict.values())]
 
-def _log_prob(logits, labels_lst, num_tokens):
-    _, sorted_indices = torch.sort(logits[:], descending=True)
+#log prob(token_j)  = logit_j - log(sum_k exp(logit_k))
+def _log_prob(logits_tensor, labels_lst, num_tokens):
+    _, sorted_indices = torch.sort(logits_tensor[:], descending=True)
     sorted_indices = list(sorted_indices.detach().numpy())
-    #sum_exp = log prob code
-    return [(tokenizer.decode([index]), str(logits[index])) for index in sorted_indices[:num_tokens]]
+    logits = logits_tensor.detach.numpy()
+    log_probs = list(logits - log(sum(exp(logits))))
+    #converting logits into log probs shouldn't change indices...
+    return [(tokenizer.decode([index]), str(log_probs[index])) for index in sorted_indices[:num_tokens]]
