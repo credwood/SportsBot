1f113adcbc9fc8c925102c6e865f14ef253526fa
Charysse <charysse.redwood@gmail.com>
2020-12-27 20:24:11 -0800

    added more vizualizations

--- sportsbot/finetune.py
+++ sportsbot/finetune.py
@@ -1,14 +1,16 @@
 import random
-from collections import defaultdict
+#from collections import defaultdict
 import os
 import torch
-import pandas as pd
+import torch.nn.functional as F
+#import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from transformers import GPT2Tokenizer, GPT2LMHeadModel, AdamW, get_linear_schedule_with_warmup
 from tqdm import tqdm
+from PIL import Image
 from IPython import display
-from IPython.display import display as dsp
-#import torch.nn.functional as F
+#from IPython.display import display as dsp
 from .inference import predict
 
 def train(
@@ -29,7 +31,7 @@ def train(
     output_prefix="gpt2_fintune",
     save_model_on_epoch=True,
     eval_between_epochs=True,
-    validation_file="validation"
+    validation_file="validation",
 ):
 
     # We can add these special tokens to the vocabulary and the embeddings of the model:
@@ -48,18 +50,35 @@ def train(
     #accumulating_batch_count = 0
     #input_tensor = None
     plt.ion()
-    fig, (ax_accuracy, ax_loss) = plt.subplots(1, 2, tight_layout=True)
+    fig, (ax_accuracy, ax_loss, ax_labels) = plt.subplots(3, 1, figsize=(15,15), tight_layout=True)
     accuracy = []
+    soft_accuracy = []
     validation_loss = []
     acc_loss = []
     running_loss = 0
-    all_model_responses = defaultdict()
+    #label_softmax = []
+    #all_model_responses = defaultdict()
+    running_label_loss = {
+                  " No": [],
+                  " Remote": [],
+                  " Unsure": [],
+                  " Probably": [],
+                  " Yes": [],
+                  " Neutral": [],
+                  " None": [],
+                  " Positive": [],
+                  " Defensive": [],
+                  " Negative": [],
+                  " Opposition": [],
+                  " Discussion": [],
+                  " Agreement": []
+    }
     for epoch in range(epochs):
 
         model.train()
         random.shuffle(dataset)
 
-        print(f"Training epoch {epoch}")
+        print(f"Training epoch {epoch+1}")
         #batched_dataset = create_batches(dataset,batch_size,max_seq_len)
         batched_dataset = [conv for conv in dataset if len(conv.template) < max_seq_len]
         for index, batch in tqdm(enumerate(batched_dataset),ascii=True, desc="current batch"):
@@ -69,8 +88,11 @@ def train(
             outputs = model(batched_tensors, labels=labels)
             loss = outputs[0]
             loss.backward()
-            loss_val = loss.item()
-            running_loss += loss_val
+            #loss_val = loss.item()
+            #running_loss += loss_val
+            logits_last_token = outputs[1][...,-2,:][:]
+            current_loss_last_token = F.cross_entropy(logits_last_token.view(-1, logits_last_token.size(-1)), labels[-1].view(-1))
+            running_loss += current_loss_last_token.item()
 
             if (index+1)%batch_size==0 or index == len(batched_dataset)-1:
                 optimizer.step()
@@ -91,28 +113,48 @@ def train(
                                     labels=validation_labels,
                                     labels_dict=labels_dict
                           )
-            model_responses = defaultdict()
-            model_responses["correct answer"] = [labels_dict[model_stats[str(index)][2]] for index in range(len(validation_set))]
-            model_responses["1st choice"] = [model_stats[str(index)][3][0][0] for index in range(len(validation_set))]
-            model_responses["2nd choice"] = [model_stats[str(index)][3][1][0] for index in range(len(validation_set))]
-            model_responses["3rd choice"] = [model_stats[str(index)][3][2][0] for index in range(len(validation_set))]
-            all_model_responses[epoch] = model_responses
+            #model_responses = defaultdict()
+            #model_responses["correct answer"] = [labels_dict["all_values"][model_stats[str(index)][2]] for index in range(len(validation_set))]
+            #model_responses["1st choice"] = [model_stats[str(index)][3][0][0] for index in range(len(validation_set))]
+            #model_responses["2nd choice"] = [model_stats[str(index)][3][1][0] for index in range(len(validation_set))]
+            #model_responses["3rd choice"] = [model_stats[str(index)][3][2][0] for index in range(len(validation_set))]
+            #all_model_responses[epoch] = model_responses
             accuracy.append(float(model_stats["accuracy"]))
+            soft_accuracy.append(float(model_stats["soft_accuracy"]))
             validation_loss.append(float(model_stats["validation_loss"]))
-            plot_loss_accuracy(accuracy, acc_loss, validation_loss, ax_accuracy, ax_loss, epoch+2, fig)
+            label_softmaxes = model_stats["label_softmaxes"]
+            for label in label_softmaxes.keys():
+                running_label_loss[label].append(label_softmaxes[label])
+            #top_model_predict = [int(model_stats[str(index)][3][0][1]) for index in range(len(validation_set))]
+            #labels_top_model_predict = [model_stats[str(index)][3][0][0] for index in range(len(validation_set))]
+            #correct_answers = [labels_dict["all_values"][model_stats[str(index)][2]] for index in range(len(validation_set))]
+            plot_loss_accuracy(accuracy,
+                                soft_accuracy,
+                                acc_loss,
+                                validation_loss,
+                                ax_accuracy,
+                                ax_loss,
+                                ax_labels,
+                                epoch+2,
+                                fig,
+                                running_label_loss,
+                                labels_dict=labels_dict
+            )
+            hist_data = model_stats["hist_data"]
+            plot_label_histograms(hist_data, epoch+1, lr, output_prefix)
         if save_model_on_epoch:
             torch.save(
                 model.state_dict(),
                 os.path.join(output_dir, f"{output_prefix}-{epoch}.pt"),
             )
-    for stat in all_model_responses:
-        top_predicts = pd.DataFrame.from_dict(all_model_responses[stat])
-        print(f"epoch {stat+1}: ")
-        dsp(top_predicts)
+    #for stat in all_model_responses:
+        #top_predicts = pd.DataFrame.from_dict(all_model_responses[stat])
+        #print(f"epoch {stat+1}: ")
+        #dsp(top_predicts)
     plt.savefig(f"loss_accuracy_graph_{output_prefix}.png")
     return model
 
-def create_batches(data, batch_size,max_seq_len):
+def create_batches(data, batch_size, max_seq_len):
     data_ = sorted(data, key=lambda x: len(x.template))
     data_ = [record for record in data_ if len(record.template) <= max_seq_len]
     data_ = [data_[index:index+batch_size] for index in range(0,len(data_), batch_size)]
@@ -136,20 +178,107 @@ def tokenize_data(conversation, tokenizer, device):
     labels = torch.LongTensor(token_labels).to(device)
     return inputs, labels
 
-def plot_loss_accuracy(updated_accuracy, updated_loss, validation_loss, ax_accuracy, ax_loss, epochs, fig):
+def plot_loss_accuracy(updated_accuracy,
+                        updated_soft_accuracy,
+                        updated_loss,
+                        validation_loss,
+                        ax_accuracy,
+                        ax_loss,
+                        ax_labels,
+                        epochs,
+                        fig,
+                        labels_softmaxes,
+                        labels_dict=None
+):
     x_axis = [count for count in range(1,epochs)]
     y_axis = updated_accuracy
+    y_axis_soft = updated_soft_accuracy
     ax_accuracy.clear()
     ax_loss.clear()
+    ax_labels.clear()
+    ax_accuracy.set_ylim([0,1])
     ax_accuracy.set_xlabel("epoch")
     ax_accuracy.set_ylabel("accuracy")
     ax_loss.set_xlabel("epoch")
     ax_loss.set_ylabel("loss")
+    ax_labels.set_xlabel("epoch")
+    ax_labels.set_ylabel("avg. softmax")
+    ax_labels.set_title("avg. softmax for each label")
     ax_accuracy.set_title("validation acccuracy")
-    ax_loss.set_title("loss")
-    ax_accuracy.plot(x_axis, y_axis, '--ro')
+    ax_loss.set_title("avg loss of final token per epoch")
+    ax_accuracy.plot(x_axis, y_axis, '--ro', label='hard accuracy')
+    ax_accuracy.plot(x_axis, y_axis_soft, '--yo', label='soft accuracy')
+    color=iter(plt.cm.rainbow(np.linspace(0,1,len(labels_softmaxes.keys()))))
+    for _, key in enumerate(labels_softmaxes.keys()):
+        next_color=next(color)
+        ax_labels.plot(x_axis, labels_softmaxes[key], c=next_color, label=f"{key}") #curr_ax =
     ax_loss.plot(x_axis, updated_loss,'--ro', label='training')
     ax_loss.plot(x_axis, validation_loss, '--bo', label='validation')
     ax_loss.legend(loc="best")
+    ax_accuracy.legend(loc="best")
+    ax_labels.legend(loc="best")
     display.clear_output(wait=True)
     display.display(plt.gcf())
+
+def plot_label_histograms(hist_data, epoch, lr, output_prefix):
+    fig_2, ax_hist = plt.subplots(tight_layout=True)
+    width = 0.35
+    ground_truth, model_predictions = hist_data[0], hist_data[1]
+    ground_truth["invalid"] = 0
+    labels = list(ground_truth.keys())
+    for key in labels:
+        if key not in model_predictions.keys():
+            model_predictions[key] = 0
+    for key in model_predictions.keys():
+        if key not in labels:
+            labels.append(key)
+            ground_truth[key] = 0
+    x_ax = np.arange(len(labels)) #label locations
+    count = max(len(ground_truth.values()), len(model_predictions.values()))
+    assert len(ground_truth.keys()) == len(model_predictions.keys()), f"{ground_truth.keys()} {model_predictions.keys()}"
+
+    model_predictions = [model_predictions[val] for val in sorted(model_predictions.keys())]
+    ground_truth = [ground_truth[val] for val in sorted(ground_truth.keys())]
+
+    ground_truth_plt = ax_hist.bar(x_ax-width/2, ground_truth, width, label='ground truth')
+    model_predictions_plt = ax_hist.bar(x_ax+width/2, model_predictions, width, label='model predictions')
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax_hist.set_ylabel('num. words')
+    num_labels = [i for i in range(count+1)]
+    ax_hist.set_yticks(num_labels)
+    ax_hist.set_yticklabels(num_labels)
+    ax_hist.set_title(f'ground truth vs. predicted words for epoch {epoch-1}')
+    ax_hist.set_xticks(x_ax)
+    ax_hist.set_xticklabels(sorted(labels))
+    ax_hist.legend()
+    autolabel(ground_truth_plt, ax_hist)
+    autolabel(model_predictions_plt, ax_hist)
+    plt.savefig(f"label_hist_{output_prefix}_{lr}_{epoch}.png")
+    plt.close(fig=fig_2)
+
+
+def autolabel(rects, axis):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    for rect in rects:
+        height = rect.get_height()
+        axis.annotate('{}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 3),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+
+def pil_grid(images, max_horiz=np.iinfo(int).max, file_name='image.png'):
+    n_images = len(images)
+    n_horiz = min(n_images, max_horiz)
+    h_sizes = [0] * n_horiz
+    v_sizes = [0] * (n_images // n_horiz) if not n_images // n_horiz else  [0] * ((n_images // n_horiz)+1)
+    for index, image in enumerate(images):
+        height, width = index % n_horiz, index // n_horiz
+        h_sizes[height] = max(h_sizes[height], image.size[0])
+        v_sizes[width] = max(v_sizes[width], image.size[1])
+    h_sizes, v_sizes = np.cumsum([0] + h_sizes), np.cumsum([0] + v_sizes)
+    im_grid = Image.new('RGB', (h_sizes[-1], v_sizes[-1]), color='white')
+    for index, image in enumerate(images):
+        im_grid.paste(image, (h_sizes[index % n_horiz], v_sizes[index // n_horiz]))
+    im_grid.save(file_name)
+    return im_grid
--- sportsbot/inference.py
+++ sportsbot/inference.py
@@ -2,7 +2,7 @@
 Functions for testing Hugginface's GPT-2 trained model
 """
 import json
-from collections import defaultdict
+from collections import defaultdict, Counter
 from numpy import exp, log
 import torch
 import torch.nn.functional as F
@@ -29,40 +29,67 @@ def predict(test_convs,
             num_top_softmax=20,
             json_file_out='add_stats_output.jsonl',
             labels=None,
-            labels_dict=None
+            labels_dict=None,
         ):
     """
     Function for finetuned model. saves and returns `Conversation` objects
     with model statistics (top SoftMax values for each conversation).
     """
-    assert  sum([len(tokenizer.encode(label)) == 1 for label in labels_dict.values()]) == len(list(labels_dict.values()))
+    assert  sum([len(tokenizer.encode(label)) == 1 for label in labels_dict["all_values"].values()]) == len(list(labels_dict["all_values"].values()))
     model.to(device)
     model.eval()
     conversations = defaultdict()
     answers = []
+    accumulated_loss = []
+    dataset_size = len(test_convs)
+    running_label_loss = {
+                  " No": 0,
+                  " Remote": 0,
+                  " Unsure": 0,
+                  " Probably": 0,
+                  " Yes": 0,
+                  " Neutral": 0,
+                  " None": 0,
+                  " Positive": 0,
+                  " Defensive": 0,
+                  " Negative": 0,
+                  " Opposition": 0,
+                  " Discussion": 0,
+                  " Agreement": 0
+    }
     for i, test_conv in enumerate(test_convs):
-        tweet_template = test_conv.template + labels_dict[labels[i]]
+        tweet_template = test_conv.template
         input_ids = tokenizer.encode(tweet_template)
+        label = labels_dict["all_values"][labels[i]]
+        label = tokenizer.encode(label)
         input_tensor = torch.LongTensor(input_ids).to(device)
+        label = torch.LongTensor(label).to(device)
         #test_conv.template += labels_dict[labels[i]]
         #tensor, labels, _ = tokenize_data(test_conv, tokenizer, device)
         with torch.no_grad():
             output = model(input_tensor,labels=input_tensor,return_dict=True)
         logits = output.logits
-        logits = logits[...,-2,:]
-        loss = output.loss.item()
+        logits = logits[...,-1,:]
+        accumulated_loss.append(F.cross_entropy(logits.view(-1, logits.size(-1)), label.view(-1)).item())
         predicted_prob = F.softmax(logits, dim=-1)
         top_softmax = _top_softmax(predicted_prob, tokenizer, num_top_softmax)
         answers.append(top_softmax[0][0])
         if labels is not None:
-            softmax_tokens = _label_softmax(predicted_prob, tokenizer, labels_dict)
-            conversations[str(i)] = [test_conv.template, softmax_tokens, labels[i], top_softmax]
+            all_label_softmax = _all_label_softmax(predicted_prob, tokenizer, labels_dict["all_values"])
+            conversations[str(i)] = [test_conv.template, all_label_softmax, labels[i], top_softmax]
         else:
-            softmax_tokens = _label_softmax(predicted_prob, tokenizer, labels_dict)
-            conversations[str(i)] = [test_conv.template, softmax_tokens, top_softmax]
+            conversations[str(i)] = [test_conv.template, top_softmax]
     if labels is not None:
-        conversations["accuracy"] = str(_calculate_accuracy(labels, answers, labels_dict))
-        conversations["validation_loss"] = loss
+        conversations["accuracy"] = str(_calculate_accuracy(labels, answers, labels_dict["all_values"]))
+        conversations["soft_accuracy"] = str(_soft_accuracy(labels, labels_dict["bucketed_labels"], answers))
+        conversations["validation_loss"] = sum(accumulated_loss)/dataset_size
+        answers_str = [answer if answer in labels_dict["all_values"].values() else "invalid" for answer in answers]
+        labels_str = [labels_dict["all_values"][label] for label in labels]
+        conversations["hist_data"] = [Counter(labels_str), Counter(answers_str)]
+        running_label_loss = _label_softmax(predicted_prob, tokenizer, labels_dict["all_values"], running_label_loss)
+        for label in labels_dict["all_values"].values():
+            running_label_loss[label] = running_label_loss[label]/dataset_size
+        conversations["label_softmaxes"] = running_label_loss
         print(f"accuracy: {conversations['accuracy']}")
     with open(json_file_out, "w") as dump:
         json.dump(conversations, dump, indent=4)
@@ -81,9 +108,16 @@ def _top_softmax(prob_dict, tokenizer, num_tokens):
     sorted_indices = list(sorted_indices.cpu().numpy())
     return [(tokenizer.decode([index]), str(prob_dict[index].item())) for index in sorted_indices[:num_tokens]]
 
-def _label_softmax(prob_dict, tokenizer, labels_dict):
+def _label_softmax(prob_dict, tokenizer, labels_dict, running_label_loss):
     prob_dict = list(prob_dict.detach().cpu().numpy())
-    return [{label: str(prob_dict[tokenizer.encode(label)[0]])} for label in list(labels_dict.values())]
+    return_dict = defaultdict()
+    for label in labels_dict.values():
+        running_label_loss[label] += prob_dict[tokenizer.encode(label)[0]]
+    return running_label_loss
+
+def _all_label_softmax(prob_dict, tokenizer, labels_dict):
+    prob_dict = list(prob_dict.detach().cpu().numpy())
+    return [{label: str(prob_dict[tokenizer.encode(label)[0]].item())} for label in labels_dict.values()]
 
 #log prob(token_j)  = logit_j - log(sum_k exp(logit_k))
 def _log_prob(logits_tensor, labels_lst, num_tokens, tokenizer):
@@ -93,3 +127,10 @@ def _log_prob(logits_tensor, labels_lst, num_tokens, tokenizer):
     log_probs = list(logits - log(sum(exp(logits))))
     #converting logits into log probs shouldn't change indices...
     return [(tokenizer.decode([index]), str(log_probs[index].item())) for index in sorted_indices[:num_tokens]]
+
+def _soft_accuracy(labels, bucketed_labels, model_answers):
+    correct = 0.
+    for i, answer in enumerate(labels):
+        if model_answers[i] in bucketed_labels[answer]:
+            correct += 1
+    return correct/len(labels)
